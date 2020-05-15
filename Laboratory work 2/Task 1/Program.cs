02d29b43b7e87670b0b89013264973b9925baa2b
Petro Kryvorotko <debianr@i.ua>
2020-05-15 21:59:15 +0300

    Laboratory work 2 code was added

---
+++ Laboratory work 2/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mult data = new Mult();
+            data.GetInputData();
+            long result = data.Multiply();
+            Console.WriteLine($"\nResult: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratory work 2/Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division division = new Division();
+            division.GetInputData();
+            division.Divide();
+            Console.WriteLine($"Fraction: {division.Fraction}\nRemainder: {division.Remainder}");
+
+            Console.ReadKey();
+        }
+    }
+}
